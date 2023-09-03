90ad5ae2efb2b2d21c23d07baf1209182733ecc5
Ezzei <andree_reus24@hotmail.com>
2023-09-03 13:57:21 -0500

    Se agrega la tarea

---
+++ src/main/java/com/cibertec/demo/Tarea01VentocillaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea01VentocillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea01VentocillaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cibertec/demo/archivos/Client.java
@@ -0,0 +1,47 @@
+package com.cibertec.demo.archivos;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	
+    private final Integer PORT = 13;
+    private final String IP = "localhost";
+     
+    
+    public Client() {
+        try {
+            Socket clientServer = new Socket(IP, PORT);
+            System.out.println("-------- 1 Iniciando comunicación con el area ----------");
+
+            // Corrige la ruta del archivo utilizando barras diagonales dobles o barras diagonales normales
+            File fileOrigen = new File("C:\\Client\\Ventocilla.txt");
+
+            FileInputStream fis = new FileInputStream(fileOrigen);
+            DataOutputStream salida = new DataOutputStream(clientServer.getOutputStream());
+
+            int byteLeidos;
+            while ((byteLeidos = fis.read()) != -1) {
+                salida.write(byteLeidos);
+            }
+
+            fis.close();
+            salida.close();
+
+            System.out.println("-------- 2 Finalizando comunicación con el area  --------");
+            clientServer.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args){
+    	new Client();
+    }
+}
